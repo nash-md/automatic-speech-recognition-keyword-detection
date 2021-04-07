44b1423f16b1d943311f2a8b09d65613e6a81ac1
Matthias Damm <mdamm@twilio.com>
2021-04-07 08:38:28 +0200

    updated package.json, removed body parser

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
-app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 
 const router = express.Router();
 
